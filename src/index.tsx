2d7d4d8fe5c04c11c58fe19af6d5318747cd57c3
xinmu <2953282238@qq.com>
2023-09-01 16:02:13 +0800

    版本恢复

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
-import App from "./App";
-
 import "./index.scss"
-
+import App from "./App";
 const root = createRoot(document.getElementById("app") as Element);
 root.render(<App />)
